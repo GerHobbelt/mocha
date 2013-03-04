a0552005718175f513293c923669cc4820547457
Ashwin Phatak <ashwinpphatak@gmail.com>
2013-03-04 14:05:58 +0530

    on demand json cov [#45368087]

--- mocha.js
+++ mocha.js
@@ -2491,6 +2491,7 @@ exports.HTMLCov = require('./html-cov');
 exports.JSONStream = require('./json-stream');
 exports.Teamcity = require('./teamcity');
 exports.OnDemandHTMLCov = require('./on-demand-html-cov');
+exports.OnDemandJSONCov = require('./on-demand-json-cov');
 
 }); // module: reporters/index.js
 
@@ -3401,7 +3402,7 @@ function OnDemandHTMLCov(runner) {
     , fn = jade.compile(str, { filename: file })
     , self = this;
 
-  process.on('SIGUSR1', function() {
+  process.on('SIGUSR2', function() {
     var mockRunner = new EventEmitter();
 
     JSONCov.call(self, mockRunner, false);
@@ -3444,6 +3445,61 @@ function coverageClass(n) {
   return 'terrible';
 }
 }); // module: reporters/on-demand-html-cov.js
+
+require.register("reporters/on-demand-json-cov.js", function(module, exports, require){
+
+/**
+ * Module dependencies.
+ */
+var EventEmitter = require('browser/events').EventEmitter
+var path = require('browser/path');
+
+var JSONCov = require('./json-cov')
+  , fs = require('browser/fs');
+
+/**
+ * Expose `OnDemandJSONCov`.
+ */
+
+exports = module.exports = OnDemandJSONCov;
+
+/**
+ * Initialize a new `JsCoverage` reporter.
+ *
+ * @param {Runner} runner
+ * @api public
+ */
+
+function OnDemandJSONCov(runner) {
+  var self = this;
+
+  process.on('SIGUSR2', function() {
+    var mockRunner = new EventEmitter();
+
+    JSONCov.call(self, mockRunner, false);
+
+    mockRunner.on('end', function() {
+      var jsonOutput = JSON.stringify(self.cov, null, 4);
+
+      // write out the JSON to a file
+      var outputFile = "coverage-report-" + process.pid + "-" + Date.now() + ".json";
+      fs.writeFileSync(path.join(process.cwd(), outputFile), jsonOutput);
+      self.emit('output', outputFile);
+    });
+
+    // trigger the output
+    mockRunner.emit('end');
+  });
+}
+
+// inherit from EventEmitter
+function F(){};
+F.prototype = EventEmitter.prototype;
+OnDemandJSONCov.prototype = new F;
+OnDemandJSONCov.prototype.constructor = OnDemandJSONCov;
+
+
+}); // module: reporters/on-demand-json-cov.js
 
 require.register("reporters/progress.js", function(module, exports, require){
 
